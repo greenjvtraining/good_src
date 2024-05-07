1e28e441315383032c19fb59ff75495c5b51d3b4
greenjvtraining <102376565+greenjvtraining@users.noreply.github.com>
2024-05-07 19:09:26 +0900

    MyController paging 코드 추가

--- jpaEx/src/main/java/com/example/jpaEx/controller/MyController.java
+++ jpaEx/src/main/java/com/example/jpaEx/controller/MyController.java
@@ -52,11 +52,31 @@ public class MyController {
 	
 	@RequestMapping("/list4")
 	public String list4(@RequestParam("pageNum") Long pageNum, Model model) {
+		//전체 게시글 수
+		int totalCnt = (int)memoRepository.count();
+		
+		//페이지 당 게시글 수 : 10개
+		int pagePerboard = 10;
+		//페이지 블록에 보여질 페이지 번호 수 : 5개
+		int pageCount = 5;
+		
+		//페이지 블록 시작 페이지 번호
+		int start = (int)Math.floor((pageNum-1)/pageCount)*pageCount + 1;
+		
+		//페이지 블록 끝 페이지 번호 (시작 번호 + 5)
+		int end = start + (pageCount - 1);
+		
+		//실제 페이지 블록 끝번호
+		int realEnd = (totalCnt / 10) + 1;
+		
 		System.out.println("pageNum : " + pageNum);
 		model.addAttribute("pageNum", pageNum);
 		//model.addAttribute("list", memoRepository.findAll());
 		model.addAttribute("list", memoRepository.findByMnoBetweenOrderByMnoDesc((pageNum*10)-9, pageNum*10));
-		model.addAttribute("cnt", memoRepository.count());
+		model.addAttribute("totalCnt", totalCnt);
+		model.addAttribute("start", start);
+		model.addAttribute("end", end);
+		model.addAttribute("realEnd", realEnd);
 		
 		return "list4";
 	}
